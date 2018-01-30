d9b305a1fdb0fc8004938326cf280ef9bac954c9
andres <andres@shuttlecloud.com>
2018-01-30 17:12:13 +0100

    version 1

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # fetch-download
-A small package to download files using the fetch interface
+A package to download files with javascript using the fetch interface
+
+
+## Usage
+
+```js
+fetchDownload=require('fetch-download');
+
+fetchDownload(url).catch((err)=>{
+    console.error(err); // {status: 404}
+});
+
+```
---
+++ main.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const FileSaver = require('file-saver');
+
+function getHeaderFilename(headers) {
+    try{
+        const rawFilename = headers.get("Content-Disposition").split("filename=")[1];
+        return rawFilename.trim().slice(1, -1); // Removes "
+    }catch(err) {
+        return "";
+    }
+}
+
+function generateErrorData(response) {
+    return {
+        status: response.status
+    };
+}
+
+function fetchDownload(url) {
+    return fetch(url, {
+        credentials: "same-origin"
+    }).then((res) => {
+        if(!res.ok) return Promise.reject(generateErrorData(res));
+        return res.blob().then((blobData) => {
+            FileSaver.saveAs(blobData, getHeaderFilename(res.headers));
+        });
+    });
+}
+
+
+
+
+module.exports = fetchDownload;
