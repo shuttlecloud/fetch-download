996895a4b1d1891d3a90a3a050ec1bad5d0a1193
Andrés <angrykoala@outlook.es>
2018-06-07 15:36:26 +0200

    typo fix

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function generateErrorData(response) {
 function fetchDownload(url, options={}) {
     return fetch(url, Object.assign({
         credentials: "same-origin"
-    }, options).then((res) => {
+    }, options)).then((res) => {
         if(!res.ok) return Promise.reject(generateErrorData(res));
         return res.blob().then((blobData) => {
             FileSaver.saveAs(blobData, getHeaderFilename(res.headers));
